ded2564eb589ecfc94fd75bf0ddc2324844e0bba
Shivam Raj <121296039+sivm-rj@users.noreply.github.com>
2025-12-26 02:51:52 +0530

    Delete embedding_utils.py

--- embedding_utils.py
+++
@@ -1,8 +0,0 @@
-
-from sentence_transformers import SentenceTransformer
-
-def load_model(name):
-    return SentenceTransformer(name)
-
-def embed(model, texts):
-    return model.encode(texts, convert_to_numpy=True)
